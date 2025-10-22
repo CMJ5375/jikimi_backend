d511e6beae7f7949d4fd283e87e6ff600cbeb9d1
limdo <ldy110322@gmail.com>
2025-10-22 22:32:12 +0900

    집나간 Q도메인이랑 30개의 게시판 Test

--- src/test/java/code/project/repository/JPostRepositoryTest.java
+++ src/test/java/code/project/repository/JPostRepositoryTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @Slf4j
@@ -50,6 +52,25 @@ class JPostRepositoryTest {
                 .build();
 
         JPostRepository.save(post);
+    }
+
+    @Test
+    void 게시글등록30개() {
+
+        JUser user = JUserRepository.findById(1L).orElseThrow();
+        List<JPost> posts = new ArrayList<>();
+        for (int i = 1; i <= 30; i++) {
+            JPost post = JPost.builder()
+                    .boardCategory(BoardCategory.FREE)
+                    .user(user)
+                    .title("복강경 수술 후기 " + i)
+                    .content("너무 아팠어요... 다신 경험하고 싶지 않아요 " + i)
+                    .createdAt(LocalDateTime.now().minusDays(30 - i)) // 날짜를 조금씩 다르게
+                    .build();
+            posts.add(post);
+        }
+
+        JPostRepository.saveAll(posts);
     }
 
     @Test
