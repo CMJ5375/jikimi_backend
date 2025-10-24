eeb68c77dbbf37333737873a978a90d816204bf2
CMJ5375 <siny0913@naver.com>
2025-10-24 17:13:36 +0900

    조회수 수정

--- src/main/java/code/project/dto/JPostDTO.java
+++ src/main/java/code/project/dto/JPostDTO.java
@@ -19,7 +19,7 @@ public class JPostDTO {
         private Integer likeCount;
         private LocalDateTime createdAt;
         private Boolean isDeleted;
-
+        private Integer viewCount;
         // 연관 객체는 ID나 이름만 노출
         private Long userId;
         private String authorName;
