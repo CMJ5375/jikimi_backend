95536205462e6954f20dd2cba530003c3b79c1a8
CMJ5375 <siny0913@naver.com>
2025-11-17 12:37:32 +0900

    AWS 배포 진짜 최종적용

--- src/main/java/code/project/util/CustomS3Util.java
+++ src/main/java/code/project/util/CustomS3Util.java
@@ -1,6 +1,7 @@
 package code.project.util;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.core.sync.RequestBody;
